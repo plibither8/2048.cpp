8eb1519f88685533bffd4fe5bda6410b329ccdbe
Mihir Chaturvedi <plibither8@gmail.com>
2018-04-15 23:17:23 +0530

    Remove inital comments

--- 2048.cpp
+++ 2048.cpp
@@ -1,28 +1,3 @@
-/*
- *
- * Game board array is defined as `board[y][x]`
- *
- * Game board will follow the following structure:
- * 'x' => x-axis OR the horizotal line OR rows
- * 'y' => y-axis OR the vertical line OR columns
- *
- * For example (zero-indexed):
- * board[2][0] refers to the 0th tile (or column) in 2nd
- * row as in this case, x = 0 and y = 2. The specific tile
- * is denoted by the '@' symbol in the following gameboard
- *
- *		+------+------+------+------+
- *		|      |      |      |      |
- *		+------+------+------+------+
- *		|      |      |      |      |
- *		+------+------+------+------+
- *		|   @  |      |      |      |
- *		+------+------+------+------+
- *		|      |      |      |      |
- *		+------+------+------+------+
- *
- */
-
 #include <iostream>
 #include <string>
 #include <iomanip>
