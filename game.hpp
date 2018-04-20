9359371131b30f53610fb55558843faac4ab4d20
Mihir Chaturvedi <plibither8@gmail.com>
2018-04-20 10:05:53 +0530

    Move `getch()` definition from game.hpp to global.hpp

--- game.hpp
+++ game.hpp
@@ -9,37 +9,6 @@
 #include <chrono>
 #include "global.hpp"
 
-#ifdef _WIN32
-
-    #include <conio.h>
-
-#else
-
-    #include <unistd.h>
-    #include <termios.h>
-    
-    char getch() {
-        char buf = 0;
-        struct termios old = {0};
-        if (tcgetattr(0, &old) < 0)
-            perror("tcsetattr()");
-        old.c_lflag &= ~ICANON;
-        old.c_lflag &= ~ECHO;
-        old.c_cc[VMIN] = 1;
-        old.c_cc[VTIME] = 0;
-        if (tcsetattr(0, TCSANOW, &old) < 0)
-            perror("tcsetattr ICANON");
-        if (read(0, &buf, 1) < 0)
-            perror("read()");
-        old.c_lflag |= ICANON;
-        old.c_lflag |= ECHO;
-        if (tcsetattr(0, TCSADRAIN, &old) < 0)
-            perror("tcsetattr ~ICANON");
-        return (buf);
-    }
-
-#endif
-
 typedef unsigned long long ull;
 enum Directions {
     UP,
