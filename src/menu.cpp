d4fc399fa9f29496df4578b4aa3a1a2987999597
cawvyoct <43756813+cawvyoct@users.noreply.github.com>
2019-02-07 21:48:02 +0100

    fix: various: separated menu text output to own functions

--- src/menu.cpp
+++ src/menu.cpp
@@ -24,24 +24,56 @@ void showScores() {
   s.printStats();
 }
 
-void input(int err) {
-  using Menu::startMenu;
+void drawMainMenuTitle() {
+  constexpr auto greetings_text = "Welcome to ";
+  constexpr auto gamename_text = "2048!";
+  constexpr auto sp = "  ";
+
+  std::ostringstream str_os;
+  std::ostringstream title_richtext;
+  title_richtext << bold_on << sp << greetings_text << blue << gamename_text
+                 << def << bold_off << "\n";
+
+  str_os << title_richtext.str();
+  std::cout << str_os.str();
+}
+
+void drawMainMenuOptions() {
+  constexpr auto menu_entry_text = R"(
+        1. Play a New Game
+        2. Continue Previous Game
+        3. View Highscores and Statistics
+        4. Exit
+
+)";
+
+  std::ostringstream str_os;
+  str_os << menu_entry_text;
+  std::cout << str_os.str();
+}
+
+void drawInputMenuPrompt(int err) {
   constexpr auto err_input_text = "Invalid input. Please try again.";
   constexpr auto prompt_choice_text = "Enter Choice: ";
   constexpr auto sp = "  ";
 
   std::ostringstream str_os;
   std::ostringstream err_input_richtext;
-  err_input_richtext << red << sp << err_input_text << def << "\n\n";
   std::ostringstream prompt_choice_richtext;
+
+  err_input_richtext << red << sp << err_input_text << def << "\n\n";
   prompt_choice_richtext << sp << prompt_choice_text;
 
   if (err) {
     str_os << err_input_richtext.str();
   }
-
   str_os << prompt_choice_richtext.str();
+
   std::cout << str_os.str();
+}
+
+void input() {
+  using Menu::startMenu;
   char c;
   std::cin >> c;
 
@@ -73,29 +105,12 @@ void input(int err) {
 namespace Menu {
 
 void startMenu(int err) {
-  constexpr auto greetings_text = "Welcome to ";
-  constexpr auto gamename_text = "2048!";
-  constexpr auto sp = "  ";
-
-  std::ostringstream str_os;
-  std::ostringstream title_richtext;
-  title_richtext << bold_on << sp << greetings_text << blue << gamename_text
-                 << def << bold_off << "\n";
-
-  constexpr auto menu_entry_text = R"(
-          1. Play a New Game
-          2. Continue Previous Game
-          3. View Highscores and Statistics
-          4. Exit
-
-)";
-
   clearScreen();
   drawAscii();
-  str_os << title_richtext.str();
-  str_os << menu_entry_text;
-  std::cout << str_os.str();
-  input(err);
+  drawMainMenuTitle();
+  drawMainMenuOptions();
+  drawInputMenuPrompt(err);
+  input();
 }
 
 } // namespace Menu
