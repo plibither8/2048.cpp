326d0b139749c90254c513808a5a2d65f63b4f6d
cawvyoct <43756813+cawvyoct@users.noreply.github.com>
2019-06-03 19:40:27 +0200

    added: game-pregamemenu TU
    Separated code in game TU into its own TU to make code much more
    readable.

---
+++ src/headers/game-pregamemenu.hpp
@@ -0,0 +1,11 @@
+#ifndef GAMEPREGAMEMENU_H
+#define GAMEPREGAMEMENU_H
+
+namespace Game {
+namespace PreGameSetup {
+void SetUpNewGame();
+void ContinueOldGame();
+} // namespace PreGameSetup
+} // namespace Game
+
+#endif
